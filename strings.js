da64095f69f7a0c33efa3f2f096ebc893103abce
Christina <Christina@Billys-MacBook-Pro.local>
2016-09-09 21:59:19 -0500

    requirements have been met, outputting to dom

--- strings.js
+++ strings.js
@@ -1,17 +1,25 @@
 let userInput = $('#inputBox');
+let output = $(".output");
 // console.log(userInput)
 function reversal(testString) {
   testString = userInput.val().toLowerCase().split("").reverse().join("");
+  output.append('<div>' + testString + '</div>');
   console.log('Reverse', testString);
 }
 
-function alphabits() {
+function alphabits(testString) {
   testString = userInput.val().toLowerCase().split("").sort().join("");
+  output.append('<div>' + testString + '</div>');
   console.log("ABC", testString);
 }
 
-function palindrome() {
+function palindrome(testString) {
   testString = userInput.val().toLowerCase().split("").reverse().join("");
+  if (userInput.val().toLowerCase() === testString) {
+    output.append('<div>' + 'Your string is a palidrome!!' + '</div>')
+  } else {
+    output.append('<div>' + 'Your string is NOT a palidrome!!' + '</div>')
+  }
 }
 
 var testString = "";
@@ -19,4 +27,11 @@ $('#submitBtn').click(function() {
   reversal(testString);
   alphabits(testString);
   palindrome(testString);
+});
+userInput.keypress(function(event){
+  if(event.keyCode === 13){
+    reversal(testString);
+    alphabits(testString);
+    palindrome(testString);
+  }
 });
