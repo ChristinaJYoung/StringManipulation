ac9f30cc2608b988984e44a3f0015b51d94dd2ba
Christina <Christina@Billys-MacBook-Pro.local>
2016-09-09 19:17:50 -0500

    sorting working in console

--- strings.js
+++ strings.js
@@ -2,11 +2,12 @@ let userInput = $('#inputBox');
 // console.log(userInput)
 function reversal(testString) {
   testString = userInput.val().toLowerCase().split("").reverse().join("");
-  console.log('Users Input', testString);
+  console.log('Reverse', testString);
 }
 
 function alphabits() {
-  testString = userInput.val().toLowerCase().split("").reverse().join("");
+  testString = userInput.val().toLowerCase().split("").sort().join("");
+  console.log("ABC", testString);
 }
 
 function palindrome() {
